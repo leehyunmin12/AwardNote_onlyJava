5aa765c01084efb224ee06136dc8493b33b480df
leehyunmin <leehm2292@gmail.com>
2025-09-22 09:25:27 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number;
+
+        while(true){
+            System.out.print("1. 등록"+"\n"+"2. 삭제"+"\n"+"3. 수정"+"\n"+"4. 나가기");
+            number = scan.nextInt();
+            if(number == 4) break; // 나가기
+            if(number == 1){
+
+            }
+
+        }
+
+    }
+}
