968fd7a36546458200bda693091c95f24a11c882
leehyunmin <leehm2292@gmail.com>
2025-10-04 21:04:02 +0900

    1004 db prct

--- src/swingtest.java
+++ src/swingtest.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class swingtest {
     public static void main(String[] args) {
         JFrame frame = new JFrame("로그인 창");
-        JPanel jPanel = new JPanel(new GridLayout(3,2,5,5));
+        JPanel panel = new JPanel(new GridLayout(3,2,5,5));
 
         JLabel idLabel = new JLabel("아이디:");
         JTextField idField = new JTextField();
@@ -13,6 +13,14 @@ public class swingtest {
         JButton loginBtn = new JButton("로그인");
         JButton cancelBtn = new JButton("취소");
 
+        panel.add(idLabel); panel.add(idField);
+        panel.add(pwField); panel.add(pwField);
+        panel.add(loginBtn); panel.add(cancelBtn);
+
+        frame.add(panel);
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
 
     }
