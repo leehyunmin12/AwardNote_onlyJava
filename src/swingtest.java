e88383d87ba708a860016675e3d41064d357710c
leehyunmin <leehm2292@gmail.com>
2025-10-05 22:25:23 +0900

    calc

--- src/swingtest.java
+++ src/swingtest.java
@@ -3,25 +3,30 @@ import java.awt.*;
 
 public class swingtest {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("로그인 창");
+        JFrame frame = new JFrame("계산기");
+        JTextField input1 = new JTextField();
+        JTextField input2 = new JTextField();
+        JButton addBtn = new JButton("+");
+        JLabel result = new JLabel("결과");
+
+        addBtn.addActionListener(e -> {
+            try{
+                int a = Integer.parseInt(input1.getText());
+                int b = Integer.parseInt(input2.getText());
+                result.setText("결과: "+ (a+b));
+            } catch (NumberFormatException ex){
+                JOptionPane.showMessageDialog(frame, "숫자를 입력하세요!");
+            }
+        });
+
         JPanel panel = new JPanel(new GridLayout(3,2,5,5));
-
-        JLabel idLabel = new JLabel("아이디:");
-        JTextField idField = new JTextField();
-        JLabel pwLabel = new JLabel("비밀번호:");
-        JPasswordField pwField = new JPasswordField();
-        JButton loginBtn = new JButton("로그인");
-        JButton cancelBtn = new JButton("취소");
-
-        panel.add(idLabel); panel.add(idField);
-        panel.add(pwField); panel.add(pwField);
-        panel.add(loginBtn); panel.add(cancelBtn);
+        panel.add(new JLabel("숫자1:")); panel.add(input1);
+        panel.add(new JLabel("숫자2:")); panel.add(input2);
+        panel.add(addBtn); panel.add(result);
 
         frame.add(panel);
         frame.setSize(300, 150);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
-
     }
 }
