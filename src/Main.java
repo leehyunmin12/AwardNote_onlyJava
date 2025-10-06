8766a655981d2c7d465c66c673e2a76f8ef6816e
leehyunmin <leehm2292@gmail.com>
2025-10-06 19:21:41 +0900

    crud db connection

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
@@ -18,83 +16,199 @@ public class Main {
 
         int number,len=0,index;
 
-        while(true){
-            System.out.println("1. 등록"+"\n"+"2. 삭제"+"\n"+"3. 수정"+"\n"+"4. 목록 보기"+"\n"+"5. 나가기");
-            number = scan.nextInt();
-            if(number == 5) break; // 나가기
-            if(number == 1){ // 등록
-                arr2[len] = len+1;
-                none = scan.nextLine();
-                System.out.print("자격증(상장) 명 ex)MosExcel : ");
-                arr[len][0] = scan.nextLine();
-                System.out.print("취득 날짜 ex)2024.05.06 : ");
-                arr[len][1] = scan.nextLine();
-                System.out.print("메모 : ");
-                arr[len][2] = scan.nextLine();
-                len++;
-                System.out.println("입력하신 값이 등록되었습니다.");
-            }
+        try {
+            // JDBC 드라이버 로드
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            while (true) {
+                System.out.println("1. 등록" + "\n" + "2. 삭제" + "\n" + "3. 수정" + "\n" + "4. 목록 보기" + "\n" + "5. 나가기");
+                number = scan.nextInt();
+                if (number == 5) break; // 나가기
 
-            if(number == 2){ // 삭제
-                System.out.println("------------------------------------------------------------------");
-                for(int i=0;i<len;i++){
-                    if(arr2[i]!=-1) {
-                        System.out.println(arr2[i]);
-                        System.out.println("자격증(상장) 명 : " + arr[i][0]);
-                        System.out.println("취득 날짜 : " + arr[i][1]);
-                        System.out.println("메모 : " + arr[i][2]);
-                        System.out.println("------------------------------------------------------------------");
-                    }
+                if (number == 1) { // 등록
+
+                    // 입력 받고 배열에 저장
+                    arr2[len] = len + 1;
+                    none = scan.nextLine();
+                    System.out.print("자격증(상장) 명 ex)MosExcel : ");
+                    arr[len][0] = scan.nextLine();
+                    System.out.print("취득 날짜 ex)2024.05.06 : ");
+                    arr[len][1] = scan.nextLine();
+                    System.out.print("메모 : ");
+                    arr[len][2] = scan.nextLine();
+
+                    // DB에 저장
+                    String sql = "INSERT INTO list (Cname, Cdate, Cmemo) VALUES (?, ?, ?)";
+                    PreparedStatement pst = connection.prepareStatement(sql);
+
+                    // 값 바인딩
+                    pst.setString(1, arr[len][0]);
+                    pst.setString(2, arr[len][1]);
+                    pst.setString(3, arr[len][2]);
+
+                    pst.executeUpdate(); // 실행
+
+                    len++;
+                    System.out.println("입력하신 값이 등록되었습니다.");
                 }
-                System.out.println("삭제할 번호를 입력하시오.");
-                int n = scan.nextInt();
-                n-=1;
-                arr2[n] = -1;
-                System.out.println("입력한 번호의 값이 삭제되었습니다.");
-            }
-            if(number == 3){
-                System.out.println("------------------------------------------------------------------");
-                for(int i=0;i<len;i++){
-                    if(arr2[i]!=-1) {
-                        System.out.println(arr2[i]);
-                        System.out.println("자격증(상장) 명 : " + arr[i][0]);
-                        System.out.println("취득 날짜 : " + arr[i][1]);
-                        System.out.println("메모 : " + arr[i][2]);
-                        System.out.println("------------------------------------------------------------------");
+
+                if (number == 2) { // 삭제
+
+//                    System.out.println("------------------------------------------------------------------");
+//                    for (int i = 0; i < len; i++) {
+//                        if (arr2[i] != -1) {
+//                            System.out.println(arr2[i]);
+//                            System.out.println("자격증(상장) 명 : " + arr[i][0]);
+//                            System.out.println("취득 날짜 : " + arr[i][1]);
+//                            System.out.println("메모 : " + arr[i][2]);
+//                            System.out.println("------------------------------------------------------------------");
+//                        }
+//                    }
+                    String sql = "SELECT * FROM list"; // 쿼리문
+                    try (PreparedStatement pst = connection.prepareStatement(sql);
+                         ResultSet rs = pst.executeQuery()) {
+
+                        while(rs.next()) {
+                            int id = rs.getInt("id");
+                            String Cn = rs.getString("Cname");
+                            String Cd = rs.getString("Cdate");
+                            String Cm = rs.getString("Cmemo");
+                            System.out.println(id);
+                            System.out.println("자격증(상장) 명 : " + Cn);
+                            System.out.println("취득 날짜 : " + Cd);
+                            System.out.println("메모 : " + Cm);
+                        }
+                    } catch (Exception e) {
+                        e.getStackTrace();
                     }
-                }
-                System.out.print("수정할 번호를 입력하시오.");
-                int n = scan.nextInt();
-                n-=1;
-                System.out.println("현재 값 : 자격증(상장) 명 : " + arr[n][0]);
-                System.out.print("수정 할 값을 입력하시오 : ");
-                none = scan.nextLine();
-                Cname = scan.nextLine();
-                arr[n][0] = Cname;
-                System.out.println("현재 값 : 취득 날짜 : " + arr[n][1]);
-                System.out.print("수정 할 값을 입력하시오 : ");
-                Cdate = scan.nextLine();
-                arr[n][1] = Cdate;
-                System.out.println("현재 값 : 메모 : " + arr[n][2]);
-                System.out.print("수정 할 값을 입력하시오 : ");
-                Cmemo = scan.nextLine();
-                arr[n][2] = Cmemo;
-                System.out.println("수정이 완료되었습니다.");
-            }
-            if(number == 4){ // 목록 보기
-                System.out.println("------------------------------------------------------------------");
-                for(int i=0;i<len;i++){
-                    if(arr2[i]!=-1) {
-                        System.out.println(arr2[i]);
-                        System.out.println("자격증(상장) 명 : " + arr[i][0]);
-                        System.out.println("취득 날짜 : " + arr[i][1]);
-                        System.out.println("메모 : " + arr[i][2]);
-                        System.out.println("------------------------------------------------------------------");
+                    System.out.println("------------------------------------------------------------------");
+
+                    System.out.println("삭제할 번호를 입력하시오.");
+                    int n = scan.nextInt();
+//                    n -= 1;
+//                    arr2[n] = -1;
+
+                    String sql2 = "DELETE FROM list WHERE id=?";
+                    try (PreparedStatement pst = connection.prepareStatement(sql2)){
+                        pst.setInt(1, n);
+                        pst.executeUpdate();
+                    } catch (Exception e){
+                        e.getStackTrace();
                     }
+
+                    System.out.println("입력한 번호의 값이 삭제되었습니다.");
+                }
+
+                if (number == 3) {
+                    /*
+                    System.out.println("------------------------------------------------------------------");
+                    for (int i = 0; i < len; i++) {
+                        if (arr2[i] != -1) {
+                            System.out.println(arr2[i]);
+                            System.out.println("자격증(상장) 명 : " + arr[i][0]);
+                            System.out.println("취득 날짜 : " + arr[i][1]);
+                            System.out.println("메모 : " + arr[i][2]);
+                            System.out.println("------------------------------------------------------------------");
+                        }
+                    }
+                    System.out.print("수정할 번호를 입력하시오.");
+                    int n = scan.nextInt();
+                    n -= 1;
+                    System.out.println("현재 값 : 자격증(상장) 명 : " + arr[n][0]);
+                    System.out.print("수정 할 값을 입력하시오 : ");
+                    none = scan.nextLine();
+                    Cname = scan.nextLine();
+                    arr[n][0] = Cname;
+                    System.out.println("현재 값 : 취득 날짜 : " + arr[n][1]);
+                    System.out.print("수정 할 값을 입력하시오 : ");
+                    Cdate = scan.nextLine();
+                    arr[n][1] = Cdate;
+                    System.out.println("현재 값 : 메모 : " + arr[n][2]);
+                    System.out.print("수정 할 값을 입력하시오 : ");
+                    Cmemo = scan.nextLine();
+                    arr[n][2] = Cmemo;
+                     */
+
+                    String sql = "SELECT * FROM list"; // 쿼리문
+                    try (PreparedStatement pst = connection.prepareStatement(sql);
+                         ResultSet rs = pst.executeQuery()) {
+
+                        while(rs.next()) {
+                            int id = rs.getInt("id");
+                            String Cn = rs.getString("Cname");
+                            String Cd = rs.getString("Cdate");
+                            String Cm = rs.getString("Cmemo");
+                            System.out.println(id);
+                            System.out.println("자격증(상장) 명 : " + Cn);
+                            System.out.println("취득 날짜 : " + Cd);
+                            System.out.println("메모 : " + Cm);
+                        }
+                    } catch (Exception e) {
+                        e.getStackTrace();
+                    }
+
+                    System.out.println("------------------------------------------------------------------");
+                    System.out.print("수정할 번호를 입력하시오. ");
+                    int id = scan.nextInt();
+                    System.out.print("새로운 자격증(상장) 명 : ");
+                    none = scan.nextLine();
+                    String nCn = scan.nextLine();
+                    System.out.print("새로운 취득 날짜 : ");
+                    String nCd = scan.nextLine();
+                    System.out.print("새로운 메모 : ");
+                    String nCm = scan.nextLine();
+
+                    String sql2 = "UPDATE list SET Cname=?, Cdate=?, Cmemo=? WHERE id=?";
+                    try (PreparedStatement pst = connection.prepareStatement(sql2)) {
+                        System.out.println(nCn+nCd+nCm);
+                        pst.setString(1, nCn);
+                        pst.setString(2, nCd);
+                        pst.setString(3, nCm);
+                        pst.setInt(4, id);
+
+                        pst.executeUpdate();
+                    } catch (Exception e){
+                        e.getStackTrace();
+                    }
+
+                    System.out.println("수정이 완료되었습니다.");
+                }
+
+                if (number == 4) { // 목록 보기
+                    System.out.println("------------------------------------------------------------------");
+                    /* for (int i = 0; i < len; i++) {
+                        if (arr2[i] != -1) {
+                            System.out.println(arr2[i]);
+                            System.out.println("자격증(상장) 명 : " + arr[i][0]);
+                            System.out.println("취득 날짜 : " + arr[i][1]);
+                            System.out.println("메모 : " + arr[i][2]);
+                            System.out.println("------------------------------------------------------------------");
+                        }
+                    } */
+                    String sql = "SELECT * FROM list"; // 쿼리문
+                    try (PreparedStatement pst = connection.prepareStatement(sql);
+                         ResultSet rs = pst.executeQuery()) {
+
+                        while(rs.next()) {
+                            int id = rs.getInt("id");
+                            String Cn = rs.getString("Cname");
+                            String Cd = rs.getString("Cdate");
+                            String Cm = rs.getString("Cmemo");
+                            System.out.println(id);
+                            System.out.println("자격증(상장) 명 : " + Cn);
+                            System.out.println("취득 날짜 : " + Cd);
+                            System.out.println("메모 : " + Cm);
+                        }
+                    } catch (Exception e) {
+                        e.getStackTrace();
+                    }
+
+                    System.out.println("------------------------------------------------------------------");
                 }
             }
+        } catch (Exception e) {
+            e.getStackTrace();
         }
-
 
     }
 }
